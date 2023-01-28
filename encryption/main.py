4f67fffecc299e3943a19774f981eae65f2de47e
ZHibaoui <86268784+ZakariaHibaoui@users.noreply.github.com>
2023-01-28 14:35:17 +0100

    Add files via upload

---
+++ encryption/main.py
@@ -0,0 +1,25 @@
+decrypted = b"abcdefghijklmnopqrstuvwxyz "
+encrypted = b"qwertyuiopasdfghjklzxcvbnm "
+
+encrypt_table = bytes.maketrans(decrypted, encrypted)
+decrypt_table = bytes.maketrans(encrypted, decrypted)
+
+result = ''
+choice = ''
+message = ''
+
+while choice != '0':
+    choice = input("\n Do you want to encrypt or decrypt the message?\n 1 to encrypt, 2 to decrypt or 0 to exit program. ")
+
+    if choice == '1':
+        message = input('\nEnter message for encryption: ')
+        result = message.translate(encrypt_table)
+        print(result + '\n\n')
+
+    elif choice == '2':
+        message = input('\nEnter message to decrypt: ')
+        result = message.translate(decrypt_table)
+        print(result + '\n\n')
+
+    elif choice != '0':
+        print('You have entered an invalid input, please try again. \n\n')
